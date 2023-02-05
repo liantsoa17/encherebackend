1be0e29ccdd11499683f5141cccadc60910b7a30
liantsoa17 <liantsurakotonirina@gmail.com>
2023-02-05 17:42:28 +0300

    commit

--- src/main/java/com/example/vehicule1/repository/CategorieRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.vehicule1.repository;
-
-import com.example.vehicule1.model.AdminToken;
-import com.example.vehicule1.model.Categorie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategorieRepo extends JpaRepository<Categorie,Integer> {
-}
--- src/main/java/com/example/vehicule1/repository/NotificationsRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.vehicule1.repository;
-
-import com.example.vehicule1.model.AdminToken;
-import com.example.vehicule1.model.Notifications;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationsRepo extends JpaRepository<Notifications,Integer> {
-}
