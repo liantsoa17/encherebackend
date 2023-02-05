4120473fed34d88cef741150aa5dc995825cc1f7
liantsoa17 <liantsurakotonirina@gmail.com>
2023-02-05 17:34:14 +0300

    pls god

--- src/main/java/com/example/vehicule1/Vehicule1Application.java
+++ src/main/java/com/example/vehicule1/Vehicule1Application.java
@@ -2,9 +2,13 @@ package com.example.vehicule1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Vehicule1Application {
+
 
     public static void main(String[] args) {
         SpringApplication.run(Vehicule1Application.class, args);
